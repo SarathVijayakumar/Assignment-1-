7d1047edcb46570d4aee55400620ba0e32abcb04
SarathVijayakumar <vgsarath1999@gmail.com>
2022-01-19 00:50:21 +0530

    new git

---
+++ javascripts/logi.js
@@ -0,0 +1,21 @@
+function validatePhoneNumber(input_str) {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/im;
+
+    return re.test(input_str);
+}
+
+function validateForm(event) {
+    var phone = document.getElementById('myform_phone').value;
+    if (!validatePhoneNumber(phone)) {
+        document.getElementById('phone_error').classList.remove('hidden');
+    } else {
+        document.getElementById('phone_error').classList.add('hidden');
+        alert(" All Validations are Successfull")
+        window.location = "index.html";
+
+       
+    }
+    event.preventDefault();
+}
+
+document.getElementById('myform').addEventListener('submit', validateForm);
